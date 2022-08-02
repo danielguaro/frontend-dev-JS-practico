aa8dd6cb862ac4758e08464fba0e79a5e573861f
Daniel Guarin <daniel.guaring24@gmail.com>
2022-08-02 16:58:16 -0500

    inclusion de menu a desplegable

---
+++ main.js
@@ -0,0 +1,11 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+// const toggleMenu = () => {
+// 	desktopMenu.classList.toggle('inactive');
+// };
+
+// menuEmail.addEventListener('click', toggleMenu);
+
+menuEmail.onclick = () => {
+	desktopMenu.classList.toggle('inactive');
+};
